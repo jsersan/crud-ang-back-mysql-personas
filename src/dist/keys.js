03a533f6bc39d8e2e24a608f397afbe31dd9e166
jsersan <jsersan@gmail.com>
2025-11-13 06:45:50 +0100

    Delete src/dist directory

--- src/dist/keys.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var database = {
-    host: 'qaou561.txemaserrano.com',
-    user: 'qaou561',
-    password: 'Jss12aoz',
-    database: 'qaou561'
-};
-exports.default = database;
